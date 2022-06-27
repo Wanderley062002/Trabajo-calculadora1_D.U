a4647d63e46b7ea9eda34987d665d4b34d814878
Wanderley062002 <108242912+Wanderley062002@users.noreply.github.com>
2022-06-26 20:21:08 -0400

    Add files via upload

---
+++ Calculadora_Diseñ.U/Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace Calculadora1_D.U
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Limpiar = new System.Windows.Forms.Button();
+            this.Raiz_cuadr = new System.Windows.Forms.Button();
+            this.Division = new System.Windows.Forms.Button();
+            this.Multiplicacion = new System.Windows.Forms.Button();
+            this.num7 = new System.Windows.Forms.Button();
+            this.num8 = new System.Windows.Forms.Button();
+            this.num9 = new System.Windows.Forms.Button();
+            this.Resta = new System.Windows.Forms.Button();
+            this.num4 = new System.Windows.Forms.Button();
+            this.num5 = new System.Windows.Forms.Button();
+            this.num6 = new System.Windows.Forms.Button();
+            this.Suma = new System.Windows.Forms.Button();
+            this.num1 = new System.Windows.Forms.Button();
+            this.num2 = new System.Windows.Forms.Button();
+            this.num3 = new System.Windows.Forms.Button();
+            this.Igual = new System.Windows.Forms.Button();
+            this.Num0 = new System.Windows.Forms.Button();
+            this.Punto = new System.Windows.Forms.Button();
+            this.Pantallaprincipal = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Limpiar
+            // 
+            this.Limpiar.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.Limpiar.Font = new System.Drawing.Font("Calibri", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Limpiar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Limpiar.Location = new System.Drawing.Point(21, 156);
+            this.Limpiar.Name = "Limpiar";
+            this.Limpiar.Size = new System.Drawing.Size(95, 67);
+            this.Limpiar.TabIndex = 0;
+            this.Limpiar.Text = "CE";
+            this.Limpiar.UseVisualStyleBackColor = false;
+            this.Limpiar.Click += new System.EventHandler(this.Limpiar_Click);
+            // 
+            // Raiz_cuadr
+            // 
+            this.Raiz_cuadr.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.Raiz_cuadr.Font = new System.Drawing.Font("Calibri", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Raiz_cuadr.Location = new System.Drawing.Point(137, 156);
+            this.Raiz_cuadr.Name = "Raiz_cuadr";
+            this.Raiz_cuadr.Size = new System.Drawing.Size(95, 67);
+            this.Raiz_cuadr.TabIndex = 1;
+            this.Raiz_cuadr.Text = "Raiz";
+            this.Raiz_cuadr.UseVisualStyleBackColor = false;
+            this.Raiz_cuadr.Click += new System.EventHandler(this.Raiz_cuadr_Click);
+            // 
+            // Division
+            // 
+            this.Division.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.Division.Font = new System.Drawing.Font("Calibri", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Division.Location = new System.Drawing.Point(254, 156);
+            this.Division.Name = "Division";
+            this.Division.Size = new System.Drawing.Size(95, 67);
+            this.Division.TabIndex = 2;
+            this.Division.Text = "/";
+            this.Division.UseVisualStyleBackColor = false;
+            this.Division.Click += new System.EventHandler(this.Division_Click);
+            // 
+            // Multiplicacion
+            // 
+            this.Multiplicacion.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.Multiplicacion.Font = new System.Drawing.Font("Calibri", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Multiplicacion.Location = new System.Drawing.Point(373, 156);
+            this.Multiplicacion.Name = "Multiplicacion";
+            this.Multiplicacion.Size = new System.Drawing.Size(95, 67);
+            this.Multiplicacion.TabIndex = 3;
+            this.Multiplicacion.Text = "X";
+            this.Multiplicacion.UseVisualStyleBackColor = false;
+            this.Multiplicacion.Click += new System.EventHandler(this.Multiplicacion_Click);
+            // 
+            // num7
+            // 
+            this.num7.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.num7.Font = new System.Drawing.Font("Calibri", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num7.Location = new System.Drawing.Point(21, 264);
+            this.num7.Name = "num7";
+            this.num7.Size = new System.Drawing.Size(95, 67);
+            this.num7.TabIndex = 4;
+            this.num7.Text = "7";
+            this.num7.UseVisualStyleBackColor = false;
+            this.num7.Click += new System.EventHandler(this.num7_Click);
+            // 
+            // num8
+            // 
+            this.num8.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.num8.Font = new System.Drawing.Font("Calibri", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num8.Location = new System.Drawing.Point(137, 264);
+            this.num8.Name = "num8";
+            this.num8.Size = new System.Drawing.Size(95, 67);
+            this.num8.TabIndex = 5;
+            this.num8.Text = "8";
+            this.num8.UseVisualStyleBackColor = false;
+            this.num8.Click += new System.EventHandler(this.num8_Click);
+            // 
+            // num9
+            // 
+            this.num9.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.num9.Font = new System.Drawing.Font("Calibri", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num9.Location = new System.Drawing.Point(254, 264);
+            this.num9.Name = "num9";
+            this.num9.Size = new System.Drawing.Size(95, 67);
+            this.num9.TabIndex = 6;
+            this.num9.Text = "9";
+            this.num9.UseVisualStyleBackColor = false;
+            this.num9.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Resta
+            // 
+            this.Resta.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.Resta.Font = new System.Drawing.Font("Calibri", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Resta.Location = new System.Drawing.Point(373, 264);
+            this.Resta.Name = "Resta";
+            this.Resta.Size = new System.Drawing.Size(95, 67);
+            this.Resta.TabIndex = 7;
+            this.Resta.Text = "-";
+            this.Resta.UseVisualStyleBackColor = false;
+            this.Resta.Click += new System.EventHandler(this.Resta_Click);
+            // 
+            // num4
+            // 
+            this.num4.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.num4.Font = new System.Drawing.Font("Calibri", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num4.Location = new System.Drawing.Point(21, 374);
+            this.num4.Name = "num4";
+            this.num4.Size = new System.Drawing.Size(95, 67);
+            this.num4.TabIndex = 8;
+            this.num4.Text = "4";
+            this.num4.UseVisualStyleBackColor = false;
+            this.num4.Click += new System.EventHandler(this.num4_Click);
+            // 
+            // num5
+            // 
+            this.num5.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.num5.Font = new System.Drawing.Font("Calibri", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num5.Location = new System.Drawing.Point(137, 374);
+            this.num5.Name = "num5";
+            this.num5.Size = new System.Drawing.Size(95, 67);
+            this.num5.TabIndex = 9;
+            this.num5.Text = "5";
+            this.num5.UseVisualStyleBackColor = false;
+            this.num5.Click += new System.EventHandler(this.num5_Click);
+            // 
+            // num6
+            // 
+            this.num6.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.num6.Font = new System.Drawing.Font("Calibri", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num6.Location = new System.Drawing.Point(254, 374);
+            this.num6.Name = "num6";
+            this.num6.Size = new System.Drawing.Size(95, 67);
+            this.num6.TabIndex = 10;
+            this.num6.Text = "6";
+            this.num6.UseVisualStyleBackColor = false;
+            this.num6.Click += new System.EventHandler(this.num6_Click);
+            // 
+            // Suma
+            // 
+            this.Suma.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.Suma.Font = new System.Drawing.Font("Calibri", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Suma.Location = new System.Drawing.Point(373, 374);
+            this.Suma.Name = "Suma";
+            this.Suma.Size = new System.Drawing.Size(95, 67);
+            this.Suma.TabIndex = 11;
+            this.Suma.Text = "+";
+            this.Suma.UseVisualStyleBackColor = false;
+            this.Suma.Click += new System.EventHandler(this.Suma_Click);
+            // 
+            // num1
+            // 
+            this.num1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.num1.Font = new System.Drawing.Font("Calibri", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num1.Location = new System.Drawing.Point(21, 490);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(95, 67);
+            this.num1.TabIndex = 12;
+            this.num1.Text = "1";
+            this.num1.UseVisualStyleBackColor = false;
+            this.num1.Click += new System.EventHandler(this.num1_Click);
+            // 
+            // num2
+            // 
+            this.num2.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.num2.Font = new System.Drawing.Font("Calibri", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num2.Location = new System.Drawing.Point(137, 490);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(95, 67);
+            this.num2.TabIndex = 13;
+            this.num2.Text = "2";
+            this.num2.UseVisualStyleBackColor = false;
+            this.num2.Click += new System.EventHandler(this.num2_Click);
+            // 
+            // num3
+            // 
+            this.num3.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.num3.Font = new System.Drawing.Font("Calibri", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num3.Location = new System.Drawing.Point(254, 490);
+            this.num3.Name = "num3";
+            this.num3.Size = new System.Drawing.Size(95, 67);
+            this.num3.TabIndex = 14;
+            this.num3.Text = "3";
+            this.num3.UseVisualStyleBackColor = false;
+            this.num3.Click += new System.EventHandler(this.num3_Click);
+            // 
+            // Igual
+            // 
+            this.Igual.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.Igual.Font = new System.Drawing.Font("Calibri", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Igual.Location = new System.Drawing.Point(373, 490);
+            this.Igual.Name = "Igual";
+            this.Igual.Size = new System.Drawing.Size(95, 164);
+            this.Igual.TabIndex = 15;
+            this.Igual.Text = "=";
+            this.Igual.UseVisualStyleBackColor = false;
+            this.Igual.Click += new System.EventHandler(this.Igual_Click);
+            // 
+            // Num0
+            // 
+            this.Num0.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.Num0.Font = new System.Drawing.Font("Calibri", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num0.Location = new System.Drawing.Point(137, 587);
+            this.Num0.Name = "Num0";
+            this.Num0.Size = new System.Drawing.Size(95, 67);
+            this.Num0.TabIndex = 16;
+            this.Num0.Text = "0";
+            this.Num0.UseVisualStyleBackColor = false;
+            this.Num0.Click += new System.EventHandler(this.Num0_Click);
+            // 
+            // Punto
+            // 
+            this.Punto.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.Punto.Font = new System.Drawing.Font("Calibri", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Punto.Location = new System.Drawing.Point(254, 587);
+            this.Punto.Name = "Punto";
+            this.Punto.Size = new System.Drawing.Size(95, 67);
+            this.Punto.TabIndex = 17;
+            this.Punto.Text = ".";
+            this.Punto.UseVisualStyleBackColor = false;
+            this.Punto.Click += new System.EventHandler(this.Punto_Click);
+            // 
+            // Pantallaprincipal
+            // 
+            this.Pantallaprincipal.BackColor = System.Drawing.Color.White;
+            this.Pantallaprincipal.Enabled = false;
+            this.Pantallaprincipal.Font = new System.Drawing.Font("Calibri", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pantallaprincipal.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Pantallaprincipal.Location = new System.Drawing.Point(12, 12);
+            this.Pantallaprincipal.Multiline = true;
+            this.Pantallaprincipal.Name = "Pantallaprincipal";
+            this.Pantallaprincipal.Size = new System.Drawing.Size(466, 122);
+            this.Pantallaprincipal.TabIndex = 18;
+            this.Pantallaprincipal.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(490, 679);
+            this.Controls.Add(this.Pantallaprincipal);
+            this.Controls.Add(this.Punto);
+            this.Controls.Add(this.Num0);
+            this.Controls.Add(this.Igual);
+            this.Controls.Add(this.num3);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.Controls.Add(this.Suma);
+            this.Controls.Add(this.num6);
+            this.Controls.Add(this.num5);
+            this.Controls.Add(this.num4);
+            this.Controls.Add(this.Resta);
+            this.Controls.Add(this.num9);
+            this.Controls.Add(this.num8);
+            this.Controls.Add(this.num7);
+            this.Controls.Add(this.Multiplicacion);
+            this.Controls.Add(this.Division);
+            this.Controls.Add(this.Raiz_cuadr);
+            this.Controls.Add(this.Limpiar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Limpiar;
+        private System.Windows.Forms.Button Raiz_cuadr;
+        private System.Windows.Forms.Button Division;
+        private System.Windows.Forms.Button Multiplicacion;
+        private System.Windows.Forms.Button num7;
+        private System.Windows.Forms.Button num8;
+        private System.Windows.Forms.Button num9;
+        private System.Windows.Forms.Button Resta;
+        private System.Windows.Forms.Button num4;
+        private System.Windows.Forms.Button num5;
+        private System.Windows.Forms.Button num6;
+        private System.Windows.Forms.Button Suma;
+        private System.Windows.Forms.Button num1;
+        private System.Windows.Forms.Button num2;
+        private System.Windows.Forms.Button num3;
+        private System.Windows.Forms.Button Igual;
+        private System.Windows.Forms.Button Num0;
+        private System.Windows.Forms.Button Punto;
+        private System.Windows.Forms.TextBox Pantallaprincipal;
+    }
+}
+
---
+++ Calculadora_Diseñ.U/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora1_D.U
+{
+    public partial class Form1 : Form
+    {
+        double primero = 0, segundo = 0;
+        string operador = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Pantallaprincipal.Text += "9";
+        }
+
+        private void Limpiar_Click(object sender, EventArgs e)
+        {
+            Pantallaprincipal.Clear();
+        }
+
+        private void Multiplicacion_Click(object sender, EventArgs e)
+        {
+            if (Pantallaprincipal.Text.Trim() == String.Empty)
+            {
+                return;
+            }
+            primero = Convert.ToDouble(Pantallaprincipal.Text);
+            operador = "*";
+            Pantallaprincipal.Clear();
+        }
+
+        private void Resta_Click(object sender, EventArgs e)
+        {
+            if (Pantallaprincipal.Text.Trim() == String.Empty)
+            {
+                return;
+            }
+            primero = Convert.ToDouble(Pantallaprincipal.Text);
+            operador = "-";
+            Pantallaprincipal.Clear();
+        }
+
+        private void Suma_Click(object sender, EventArgs e)
+        {
+            if (Pantallaprincipal.Text.Trim() == String.Empty)
+            {
+                return;
+            }
+            primero = Convert.ToDouble(Pantallaprincipal.Text);
+            operador = "+";
+            Pantallaprincipal.Clear();
+        }
+
+        private void Raiz_cuadr_Click(object sender, EventArgs e)
+        {
+            Pantallaprincipal.Text = (Math.Sqrt(Convert.ToDouble(Pantallaprincipal.Text))).ToString();
+        }
+
+        private void Num0_Click(object sender, EventArgs e)
+        {
+            Pantallaprincipal.Text += "0";
+        }
+
+        private void num1_Click(object sender, EventArgs e)
+        {
+            Pantallaprincipal.Text += "1";
+        }
+
+        private void num2_Click(object sender, EventArgs e)
+        {
+            Pantallaprincipal.Text += "2";
+        }
+
+        private void num3_Click(object sender, EventArgs e)
+        {
+            Pantallaprincipal.Text += "3";
+        }
+
+        private void num4_Click(object sender, EventArgs e)
+        {
+            Pantallaprincipal.Text += "4";
+        }
+
+        private void num5_Click(object sender, EventArgs e)
+        {
+            Pantallaprincipal.Text += "5";
+        }
+
+        private void num6_Click(object sender, EventArgs e)
+        {
+            Pantallaprincipal.Text += "6";
+        }
+
+        private void num7_Click(object sender, EventArgs e)
+        {
+            Pantallaprincipal.Text += "7";
+        }
+
+        private void num8_Click(object sender, EventArgs e)
+        {
+            Pantallaprincipal.Text += "8";
+        }
+
+        private void Punto_Click(object sender, EventArgs e)
+        {
+            Pantallaprincipal.Text += ".";
+        }
+
+        private void Igual_Click(object sender, EventArgs e)
+        {
+            if (Pantallaprincipal.Text.Trim() == String.Empty)
+            {
+                return;
+            }
+            segundo = Convert.ToDouble(Pantallaprincipal.Text);
+            switch (operador)
+            {
+                case "+":
+                    Pantallaprincipal.Text = (primero + segundo).ToString();
+                    break;
+                case "-":
+                    Pantallaprincipal.Text = (primero - segundo).ToString();
+                    break;
+                case "*":
+                    Pantallaprincipal.Text = (primero * segundo).ToString();
+                    break;
+                case "/":
+                    Pantallaprincipal.Text = (primero / segundo).ToString();
+                    break;
+            }
+
+        }
+
+        private void Division_Click(object sender, EventArgs e)
+        {
+            if (Pantallaprincipal.Text.Trim() == String.Empty)
+            {
+                return;
+            }
+            primero = Convert.ToDouble(Pantallaprincipal.Text);
+            operador = "/";
+            Pantallaprincipal.Clear();
+        }
+        
+    }
+}
